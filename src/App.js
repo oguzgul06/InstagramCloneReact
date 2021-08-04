bc5934d5297dcde2e6b048df54bd713e96ee5008
oguz gül <ogzgl95@outlook.com>
2021-08-04 11:30:39 +0300

    Create header and post header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import "./App.css";
+import Post from "./Post";
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
+      <div className="app__header">
+        <img className="app__headerImage" src="./image/logo.png" alt="" />
+      </div>
       <h1>Instagram Clone App</h1>
+      <Post />
     </div>
   );
 }
